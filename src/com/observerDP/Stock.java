d27625ff91bb5f974ebe6ad18c7a530e4c36784f
Praveen Kishor <praveen.kishor123@gmail.com>
2016-12-09 23:30:44 +0530

    observer pattern

--- src/com/observerDP/Stock.java
+++ src/com/observerDP/Stock.java
@@ -1,6 +1,7 @@
 package com.observerDP;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public abstract class Stock implements Observable {
@@ -14,6 +15,20 @@ public abstract class Stock implements Observable {
 	private String name;
 	private int price;
 	
+	public String getStockName() {
+		return name;
+	}
 	
+	@Override
+	public void registerObserver(Observer observer) {
+		if(observer != null) {
+			this.observers.add(observer);
+		}
+	}
+	
+	@Override
+	public void notifyObservers() {
+		Iterator<Observer> it = observers.iterator();
+	}
 
 }
