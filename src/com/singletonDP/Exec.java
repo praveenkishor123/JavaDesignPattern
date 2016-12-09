87506c6159b943add09da4799bea44f475612010
Praveen Kishor <praveen.kishor123@gmail.com>
2016-12-09 23:00:45 +0530

    getting instance through singleton

--- src/com/singletonDP/Exec.java
+++ src/com/singletonDP/Exec.java
@@ -5,9 +5,9 @@ public class Exec {
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
 			
-			Satellite s = new Satellite();
+		//	Satellite s = new Satellite();
 			
-			System.out.println(s.hashCode());
+			System.out.println(Satellite.getSatellite().hashCode());
 			
 		}
 	}
