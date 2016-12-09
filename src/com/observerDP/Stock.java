798daeedd1cf96f2033b19565f45e8e8932be9ea
Praveen Kishor <praveen.kishor123@gmail.com>
2016-12-09 23:36:42 +0530

    observer pattern

--- src/com/observerDP/Stock.java
+++ src/com/observerDP/Stock.java
@@ -29,6 +29,37 @@ public abstract class Stock implements Observable {
 	@Override
 	public void notifyObservers() {
 		Iterator<Observer> it = observers.iterator();
+		while(it.hasNext()) {
+			Observer observer = it.next();
+			observer.update(this);
+		}
 	}
-
+	
+	
+	@Override
+	public void removeObserver(Observer observer) {
+		if(observer != null) {
+			this.observers.remove(observer);
+		}
+	}
+	
+	
+	
+	public int getStockPrice() {
+		return price;
+	}
+	
+	public void updateStockPrice(int updatedPrice) {
+		
+		this.price = updatedPrice;
+		this.notifyObservers();
+	}
+	
+	
+	public void setName(String stockName) {
+		this.name = stockName;
+	}
+	
+	
 }
+ 
